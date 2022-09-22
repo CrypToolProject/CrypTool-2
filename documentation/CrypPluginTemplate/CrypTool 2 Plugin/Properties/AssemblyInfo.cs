47e5d0f9b2321a7344d32b4f8849d4eb0eceb4c2
Nils Kopal <nils@kopaldev.de>
2022-09-22 11:23:43 +0200

    CrypTool 2 Plugin template: * fixed AssemblyInfo.cs: guid has to be in quotation marks

--- documentation/CrypPluginTemplate/CrypTool 2 Plugin/Properties/AssemblyInfo.cs
+++ documentation/CrypPluginTemplate/CrypTool 2 Plugin/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid($guid1$)]
+[assembly: Guid("$guid1$")]
 
 // Version information for an assembly consists of the following four values:
 //
