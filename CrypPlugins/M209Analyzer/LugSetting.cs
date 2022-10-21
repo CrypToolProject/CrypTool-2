5636cf6b7f25d4dbf38a4f4357a97b79522d32c6
Josef Matwich <josef95@outlook.de>
2022-10-21 09:24:39 +0200

    PinSettings randomization and transformation finished. LugSettings randomization finished.

--- CrypPlugins/M209Analyzer/LugSetting.cs
+++ CrypPlugins/M209Analyzer/LugSetting.cs
@@ -15,25 +15,68 @@ namespace M209Analyzer
             "20","25","25","05","05","05","05","05","05"
         };
 
+        private Random Randomizer = new Random();
+
+        /// <summary>
+        /// Generate a random setting for the lugs.
+        /// </summary>
         public void Randomize()
         {
-
+            for(int i = 0; i < this.Bar.Length; i++)
+            {
+                this.Bar[i] = $"{this.Randomizer.Next(0, 9)}{this.Randomizer.Next(0, 9)}";
+            }
         }
 
+        /// <summary>
+        /// Get neighbor lug settings.
+        /// </summary>
+        /// <param name="V">Operation instruction version</param>
+        /// <returns></returns>
         public LugSetting[] GetNeighborLugs(string V)
         {
             // variable neighborhood approach
             return new LugSetting[] {new LugSetting() };
         }
 
+        /// <summary>
+        /// These simple transformations consist of reducing the count of one type of bars, and increasing the count of another type.
+        /// On on bar a lug position gets increased on another bar a lug position gets decreased.
+        /// </summary>
         public void ApplyTransformationSimple()
         {
-            // These simple transformations consist of reducing the count of one type of bars, and increasing the count of another type.
+            // Choose random bar and lug
+            int ChoosenBar = this.Randomizer.Next(0, 26);
+            int ChoosenLug = this.Randomizer.Next(0, 1);
+
+            StringBuilder LugString = new StringBuilder(this.Bar[ChoosenBar]);
+            
+            int LugPosition = (int)Char.GetNumericValue(this.Bar[ChoosenBar][ChoosenLug]);
+            if (LugPosition < 9)
+            {
+                LugString[ChoosenLug] = (char)(LugPosition + 1);
+                this.Bar[ChoosenBar] = LugString.ToString();
+            }
+
+            // Choose another random bar and lug
+            ChoosenBar = this.Randomizer.Next(0, 26);
+            ChoosenLug = this.Randomizer.Next(0, 1);
+
+            LugString = new StringBuilder(this.Bar[ChoosenBar]);
+
+            LugPosition = (int)Char.GetNumericValue(this.Bar[ChoosenBar][ChoosenLug]);
+            if (LugPosition > 0){
+                LugString[ChoosenLug] = (char)(LugPosition - 1);
+                this.Bar[ChoosenBar] = LugString.ToString();
+            }
         }
 
+        /// <summary>
+        /// Those consist of reducing the count of two types of bar, and increasing the counts of two other types.
+        /// </summary>
         public void ApplyTransformationComplex()
         {
-            // Those consist of reducing the count of two types of bar, and increasing the counts of two other types.
+            
         }
     }
 }
--- CrypPlugins/M209Analyzer/PinSetting.cs
+++ CrypPlugins/M209Analyzer/PinSetting.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CrypTool.M209Analyzer.Enum;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,7 @@ namespace M209Analyzer
 {
     internal class PinSetting
     {
-        public string[] Wheels = new string[6] {
+        private string[] Wheels = new string[6] {
             "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
             "ABCDEFGHIJKLMNOPQRSTUVXYZ",    // no W
             "ABCDEFGHIJKLMNOPQRSTUVX",      // no WYZ
@@ -17,8 +18,45 @@ namespace M209Analyzer
             "ABCDEFGHIJKLMNOPQ"             // no R-Z
         };
 
+        private bool[][] WheelPins = new bool[][]
+        {
+            new bool[26] { true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true },
+            new bool[25] { true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true },
+            new bool[23] { true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true },
+            new bool[21] { true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true },
+            new bool[19] { true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true },
+            new bool[17] { true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true }
+        };
+
+        private Random Randomizer = new Random();
+
+        public string[] WheelsSettings()
+        {
+            string[] wheelsSettings = new string[] { };
+            for (int i = 0; i < this.Wheels.Length; i++)
+            {
+                string wheelSetting = "";
+                for (int k = 0; k < this.Wheels[i].Length; k++)
+                {
+                    if (this.WheelPins[i][k])
+                    {
+                        wheelSetting += this.Wheels[i][k];
+                    }
+                }
+                wheelsSettings.Append(wheelSetting);
+            }
+            return wheelsSettings;
+        }
+
         public void Randomize()
         {
+            for (int i = 0; i < this.WheelPins.Length; i++)
+            {
+                for (int k = 0; k < this.WheelPins[i].Length; k++)
+                {
+                    this.WheelPins[i][k] = this.Randomizer.Next(0,1) == 1;
+                }
+            }
 
         }
 
@@ -27,8 +65,93 @@ namespace M209Analyzer
 
         }
 
-        public void ApplyTransformation()
+        public void ApplyTransformation(PinSettingTransformation pinSettingTransformation)
         {
+            switch (pinSettingTransformation)
+            {
+                case PinSettingTransformation.ToggleSingleWheelSinglePin:
+                    this.ToggleSingleWheelSinglePinTransformation();
+                    break;
+                case PinSettingTransformation.ToggleSingleWheelTwoPins:
+                    this.ToggleSingleWheelTwoPinsTransformation();
+                    break;
+                case PinSettingTransformation.ToggleTwoWheelsTwoPins:
+                    this.ToggleTwoWheelsTwoPinsTransformation();
+                    break;
+                case PinSettingTransformation.ToggleSingleWheelAllPins:
+                    this.ToggleSingleWheelAllPinsTransformation();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Toggle the state of a single wheel pin (change its state from 0 to 1, or from 1 to 0).
+        /// </summary>
+        public void ToggleSingleWheelSinglePinTransformation()
+        {
+            int randomWheel = this.Randomizer.Next(0, 5);
+            int randomPin = this.Randomizer.Next(0, this.WheelPins[randomWheel].Length - 1);
+            this.WheelPins[randomWheel][randomPin] = !this.WheelPins[randomWheel][randomPin];
+        }
+
+        /// <summary>
+        /// Toggle the state of a pair of pins, from the same wheel, which have dierent states (one is 0
+        /// and the other is 1, or vice versa).
+        /// </summary>
+        public void ToggleSingleWheelTwoPinsTransformation()
+        {
+            int randomWheel = this.Randomizer.Next(0, 5);
+            int randomPin1 = this.Randomizer.Next(0, this.WheelPins[randomWheel].Length - 1);
+            this.WheelPins[randomWheel][randomPin1] = !this.WheelPins[randomWheel][randomPin1];
+
+            int randomPin2 = this.Randomizer.Next(0, this.WheelPins[randomWheel].Length - 1);
+            // Maybe it could be better to run systematically throug the pins...
+            while (this.WheelPins[randomWheel][randomPin2] != this.WheelPins[randomWheel][randomPin1])
+            {
+                randomPin2 = this.Randomizer.Next(0, this.WheelPins[randomWheel].Length - 1);
+            }
+            this.WheelPins[randomWheel][randomPin2] = !this.WheelPins[randomWheel][randomPin2];
+        }
+
+        /// <summary>
+        /// Toggle the state of a pair of two pins, from different wheels, which have differerent states.
+        /// </summary>
+        public void ToggleTwoWheelsTwoPinsTransformation()
+        {
+            int randomWheel1 = this.Randomizer.Next(0, 5);
+            int randomPin1 = this.Randomizer.Next(0, this.WheelPins[randomWheel1].Length - 1);
+            this.WheelPins[randomWheel1][randomPin1] = !this.WheelPins[randomWheel1][randomPin1];
+
+            int randomWheel2 = this.Randomizer.Next(0, 5);
+            while (randomWheel2 == randomWheel1)
+            {
+                randomWheel2 = this.Randomizer.Next(0, 5);
+            }
+
+            int randomPin2 = this.Randomizer.Next(0, this.WheelPins[randomWheel2].Length - 1);
+            while (this.WheelPins[randomWheel2][randomPin2] != this.WheelPins[randomWheel1][randomPin1])
+            {
+                randomPin2 = this.Randomizer.Next(0, this.WheelPins[randomWheel2].Length - 1);
+            }
+            this.WheelPins[randomWheel2][randomPin2] = !this.WheelPins[randomWheel2][randomPin2];
+
+        }
+
+        /// <summary>
+        /// Toggle the state of all the pins of a wheel.
+        /// </summary>
+        public void ToggleSingleWheelAllPinsTransformation()
+        {
+            for (int i = 0; i < this.WheelPins.Length; i++)
+            {
+                for (int k = 0; k < this.WheelPins[i].Length; k++)
+                {
+                    this.WheelPins[i][k] = !this.WheelPins[i][k];
+                }
+
+            }
 
         }
     }
