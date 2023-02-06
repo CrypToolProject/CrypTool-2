17432d1fee9b40886a1c01c93dd3fa5e6a148db3
Nils Kopal <nils@kopaldev.de>
2023-02-06 15:04:26 +0100

    GRANIT/160 cipher: * created component, online documentation, and template for the GDR cipher GRANIT/160, which was used by Günter Guillaume

---
+++ CrypPlugins/GRANIT160Cipher/GRANIT160CipherSettings.cs
@@ -0,0 +1,76 @@
+﻿/*
+   Copyright 2023 Nils Kopal <Nils.Kopal<at>CrypTool.org
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+using CrypTool.PluginBase;
+using CrypTool.PluginBase.Miscellaneous;
+using System.ComponentModel;
+
+namespace CrypTool.Plugins.GRANIT160Cipher
+{
+    public enum Action
+    {
+        Encrypt,
+        Decrypt
+    }
+
+    public class GRANIT160CipherSettings : ISettings
+    {
+        #region Private Variables
+
+        private Action _action = Action.Encrypt;
+
+        #endregion
+
+        public event TaskPaneAttributeChangedHandler TaskPaneAttributeChanged;
+
+        #region TaskPane Settings
+
+        [TaskPane("ActionCaption", "ActionTooltip", null, 1, false, ControlType.ComboBox, new string[] { "Encrypt", "Decrypt" })]
+        public Action Action
+        {
+            get
+            {
+                return _action;
+            }
+            set
+            {
+                if (_action != value)
+                {
+                    _action = value;
+                    OnPropertyChanged(nameof(Action));
+                }
+            }
+        }
+
+        #endregion
+
+        #region Events
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            EventsHelper.PropertyChanged(PropertyChanged, this, propertyName);
+        }
+
+        #endregion
+
+        public void Initialize()
+        {
+        }
+
+
+    }
+}
