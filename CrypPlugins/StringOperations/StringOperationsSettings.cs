e6bb7b8f9d5eac00f33f04a3cf22ac08ebeedd33
Nils Kopal <nils@kopaldev.de>
2023-02-21 10:29:56 +0100

    StringOperations: * added operation that allows to shuffle a string -> useful for permuting alphabets

--- CrypPlugins/StringOperations/StringOperationsSettings.cs
+++ CrypPlugins/StringOperations/StringOperationsSettings.cs
@@ -74,7 +74,7 @@ namespace StringOperations
         }
 
         [TaskPane("OperationCaption", "OperationTooltip", null, 1, false, ControlType.ComboBox,
-            new[] { "OperationList1", "OperationList2", "OperationList3", "OperationList4", "OperationList5", "OperationList6", "OperationList7", "OperationList8", "OperationList9", "OperationList10", "OperationList11", "OperationList12", "OperationList13", "OperationList14", "OperationList15", "OperationList16", "OperationList17" })]
+            new[] { "OperationList1", "OperationList2", "OperationList3", "OperationList4", "OperationList5", "OperationList6", "OperationList7", "OperationList8", "OperationList9", "OperationList10", "OperationList11", "OperationList12", "OperationList13", "OperationList14", "OperationList15", "OperationList16", "OperationList17", "OperationList18" })]
         public StringOperationType Operation
         {
             get => _stringOperationType;
@@ -208,6 +208,7 @@ namespace StringOperations
         Reverse,
         Sort,
         Distinct,
-        LevenshteinDistance
+        LevenshteinDistance,
+        Shuffle
     }
 }
